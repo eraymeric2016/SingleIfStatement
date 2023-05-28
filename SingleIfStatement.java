59a4ad95e32e017000d46565c44226c6588ba239
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:35:21 +0300

    Add files via upload

---
+++ SingleIfStatement.java
@@ -0,0 +1,19 @@
+package week_04;
+
+public class SingleIfStatement {
+    public static void main(String[] args) {
+        int num=12;
+        if (num>0){
+
+            System.out.println(num + " is possitive");
+
+        }
+
+
+
+
+
+
+
+    }
+}
